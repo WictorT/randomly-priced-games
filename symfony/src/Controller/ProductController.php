a5b92dc9cadaad2d77d5b56a178237e8360ee3d2
vtimoftii <victoritmoftii@yahoo.com>
2018-07-15 17:52:44 +0300

    Get product action added

--- symfony/src/Controller/ProductController.php
+++ symfony/src/Controller/ProductController.php
@@ -1,13 +1,13 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Handler\ProductHandler;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends Controller
 {
@@ -23,7 +23,7 @@ class ProductController extends Controller
     }
 
     /**
-     * @Route(path="/products", name="app.products.list", methods={"GET"})
+     * @Rest\Get(path="/products", name="app.products.list")
      *
      * @Rest\QueryParam(name="page", nullable=true, requirements="[1-9][0-9]*", strict=true, description="page", default="1")
      * @Rest\QueryParam(name="per_page", nullable=true, requirements="[1-9][0-9]*", strict=true, description="products per page", default="3")
@@ -31,7 +31,7 @@ class ProductController extends Controller
      * @param ParamFetcherInterface $paramFetcher
      * @return JsonResponse
      */
-    public function index(ParamFetcherInterface $paramFetcher)
+    public function indexAction(ParamFetcherInterface $paramFetcher)
     {
         $products = $this->productHandler->getPaginated(
             $paramFetcher->get('page'),
@@ -39,5 +39,16 @@ class ProductController extends Controller
         );
 
         return $this->json($products, Response::HTTP_OK);
+    }
+
+    /**
+     * @Rest\Get(path="/products/{id}", name="app.products.get", requirements={"id":"\d+"})
+     *
+     * @param Product $product
+     * @return JsonResponse
+     */
+    public function getAction(Product $product)
+    {
+        return $this->json($product, Response::HTTP_OK);
     }
 }
