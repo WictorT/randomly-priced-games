75c5b2c80da1d31503f7efec8daa9da6e56785e4
victortimoftii <v.timoftii@developmentaid.org>
2018-07-20 21:15:21 +0300

    Public access allowed to product view actions

--- symfony/src/Controller/UserController.php
+++ symfony/src/Controller/UserController.php
@@ -3,12 +3,12 @@ namespace App\Controller;
 
 use App\DTO\UserDTO;
 use App\Handler\UserHandler;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 /**
@@ -28,7 +28,7 @@ class UserController extends Controller
     }
 
     /**
-     * @Route(path="/api/sign-up", name="app.users.sign_up", methods={"POST"})
+     * @Rest\Route(path="/api/sign-up", name="app.users.sign_up", methods={"POST"})
      * @ParamConverter("userDTO", converter="fos_rest.request_body")
      *
      * @param UserDTO $userDTO
