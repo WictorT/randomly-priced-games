79c68a54afabfc05df9f5737c3a18fa5f33accee
vtimoftii <victoritmoftii@yahoo.com>
2018-07-21 22:31:59 +0300

    Database specification removed from migrations

--- symfony/src/Migrations/Version20180715122624.php
+++ symfony/src/Migrations/Version20180715122624.php
@@ -34,11 +34,11 @@ final class Version20180715122624 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql("INSERT INTO rpg.rpg_products (id, name, price, created_at, updated_at) VALUES (1, 'Fallout', 1.99, '2018-07-15 14:24:57', '2018-07-15 14:24:57');");
-        $this->addSql("INSERT INTO rpg.rpg_products (id, name, price, created_at, updated_at) VALUES (2, 'Don’t Starve', 2.99, '2018-07-15 14:24:57', '2018-07-15 14:24:57');");
-        $this->addSql("INSERT INTO rpg.rpg_products (id, name, price, created_at, updated_at) VALUES (3, 'Baldur’s Gate', 3.99, '2018-07-15 14:24:57', '2018-07-15 14:24:57');");
-        $this->addSql("INSERT INTO rpg.rpg_products (id, name, price, created_at, updated_at) VALUES (4, 'Icewind Dale', 4.99, '2018-07-15 14:24:57', '2018-07-15 14:24:57');");
-        $this->addSql("INSERT INTO rpg.rpg_products (id, name, price, created_at, updated_at) VALUES (5, 'Bloodborne', 5.99, '2018-07-15 14:24:57', '2018-07-15 14:24:57');");
+        $this->addSql("INSERT INTO rpg_products (id, name, price, created_at, updated_at) VALUES (1, 'Fallout', 1.99, '2018-07-15 14:24:57', '2018-07-15 14:24:57');");
+        $this->addSql("INSERT INTO rpg_products (id, name, price, created_at, updated_at) VALUES (2, 'Don’t Starve', 2.99, '2018-07-15 14:24:57', '2018-07-15 14:24:57');");
+        $this->addSql("INSERT INTO rpg_products (id, name, price, created_at, updated_at) VALUES (3, 'Baldur’s Gate', 3.99, '2018-07-15 14:24:57', '2018-07-15 14:24:57');");
+        $this->addSql("INSERT INTO rpg_products (id, name, price, created_at, updated_at) VALUES (4, 'Icewind Dale', 4.99, '2018-07-15 14:24:57', '2018-07-15 14:24:57');");
+        $this->addSql("INSERT INTO rpg_products (id, name, price, created_at, updated_at) VALUES (5, 'Bloodborne', 5.99, '2018-07-15 14:24:57', '2018-07-15 14:24:57');");
     }
 
     public function down(Schema $schema) : void
