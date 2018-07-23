edc96ce570114245d62bd604db775cbac4103cde
victortimoftii <v.timoftii@developmentaid.org>
2018-07-23 14:35:34 +0300

    fixtures excluded from prod

--- symfony/setup.sh
+++ symfony/setup.sh
@@ -10,7 +10,11 @@ composer install;
 
 bin/console doctrine:database:create --if-not-exists --env=${environment};
 bin/console doctrine:migrations:migrate --no-interaction --env=${environment};
+
+if [ "$environment" != "prod" ]
+  then
 bin/console doctrine:fixtures:load --no-interaction --env=${environment};
+fi
 
 HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
 setfacl -R -m u:"$HTTPDUSER":rwX -m u:`whoami`:rwX var/cache var/log config/jwt/
