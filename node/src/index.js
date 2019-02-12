747eae6277795b4039d8343daf1a5496a28d2da1
hi-dima <dima@example.com>
2019-02-12 15:11:43 +0200

    Improve redux

--- node/src/index.js
+++ node/src/index.js
@@ -11,7 +11,7 @@ import rootReducer from "./store/reducers/rootReducer";
 
 const composeEnhancers =
     typeof window === 'object' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && process.env.NODE_ENV === 'development' ?
         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
         }) : compose;
 
